5a197bbe96be1e893f1ae7381d22c83740e46a39
isa <isatagirov01@mail.ru>
2023-10-15 17:50:37 +0300

    lesson php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = "Isa";
+$age = "22";
+
+echo "Hello $name your age is $age", "<br>";
+
+$a = 100;
+$b = 200;
+echo $a + $b, "<br>";
+
+$c = 100;
+$d = 50;
+echo $c * $d, "<br>";
+
+$e = 300;
+$f = 60;
+echo $e / $f, "<br>";
